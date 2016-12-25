5bc795f54bb4bbd19baedbb7bb7dffdec0d8af77
dkgr_ser <dkgr_ser@outlook.com>
2016-12-25 22:48:58 +0200

    Updated HeuristicPlayer so it uses all new code in PartC's platform.

--- src/gr/auth/ee/dsproject/crush/player/HeuristicPlayer.java
+++ src/gr/auth/ee/dsproject/crush/player/HeuristicPlayer.java
@@ -65,46 +65,7 @@ public class HeuristicPlayer implements AbstractPlayer
     {
         int[] move = availableMoves.get(findBestMoveIndex(availableMoves, board));
 
-        return calculateNextMove(move);
-    }
-
-    int[] calculateNextMove (int[] move)
-    {
-	    int[] returnedMove = new int[4];
-	
-	    if (move[2] == CrushUtilities.UP) {
-	    // System.out.println("UP");
-	        returnedMove[0] = move[0];
-	        returnedMove[1] = move[1];
-	        returnedMove[2] = move[0];
-	        returnedMove[3] = move[1] + 1;
-	    }
-	    
-	    if (move[2] == CrushUtilities.DOWN) {
-	    // System.out.println("DOWN");
-	        returnedMove[0] = move[0];
-	        returnedMove[1] = move[1];
-	        returnedMove[2] = move[0];
-	        returnedMove[3] = move[1] - 1;
-	    }
-	    
-	    if (move[2] == CrushUtilities.LEFT) {
-	    // System.out.println("LEFT");
-	        returnedMove[0] = move[0];
-	        returnedMove[1] = move[1];
-	        returnedMove[2] = move[0] - 1;
-	        returnedMove[3] = move[1];
-	    }
-	    
-	    if (move[2] == CrushUtilities.RIGHT) {
-	    // System.out.println("RIGHT");
-	       returnedMove[0] = move[0];
-	       returnedMove[1] = move[1];
-	       returnedMove[2] = move[0] + 1;
-	       returnedMove[3] = move[1];
-	    }
-	
-	    return returnedMove;
+        return CrushUtilities.calculateNextMove(move);
     }
 
     int findBestMoveIndex (ArrayList<int[]> availableMoves, Board board)
@@ -114,16 +75,12 @@ public class HeuristicPlayer implements AbstractPlayer
 	  
 	    for (int i = 0; i < availableMoves.size(); i++) {
 		   	double moveScore = moveEvaluation(availableMoves.get(i), board);
-		  	
-		  	//System.out.println("Move score: " + moveScore);
-	  	
+		  		  	
 		  	if (moveScore > max) {
 		  		max = moveScore;
 		  	    max_i = i;
 		  	}
         }
-  
-        //System.out.println("Selected move score: " + max);
 	  
 	    return max_i;	  
     }
@@ -137,7 +94,7 @@ public class HeuristicPlayer implements AbstractPlayer
      * -CandiesRemovedHeuristic on VERY_HIGH
      * -DistanceFromTopHeuristic on VERY_LOW
      *
-     * @param move A move to be evaluted in the form of {x, y, direction}
+     * @param move A move to be evaluated in the form of {x, y, direction}
      * 			 array.
      * @param board The board on which the move will be evaluated.
      * @return A value between 0.0 and less than 100.0 indicating how good a move is.
@@ -147,10 +104,10 @@ public class HeuristicPlayer implements AbstractPlayer
     {
 	    HeuristicsEngine engine = new HeuristicsEngine(new SliderMathModel(2.0));
 	  	  
-	    int[] destTile = getMoveDestination(move);
-	  
-	    Tile tile1 = board.getTile(move[0], move[1]);
-	    Tile tile2 = board.getTile(destTile[0], destTile[1]);
+	    int[] dirMove = CrushUtilities.calculateNextMove(move);
+	    
+	    Tile tile1 = board.getTile(dirMove[0], dirMove[1]);
+	    Tile tile2 = board.getTile(dirMove[2], dirMove[3]);
 	    PlayerMove pMove = new PlayerMove(tile1, tile2);
 	  
 	    engine.add(new CandiesRemovedHeuristic(pMove, board), SliderMathModel.VERY_HIGH);
@@ -160,41 +117,41 @@ public class HeuristicPlayer implements AbstractPlayer
     }
 
   
-//==== Private Static Methods ====
-  
-    /** 
-     * getMoveDestination() calculates the cords of the block next to the
-     * given one, on the given direction.
-     * 
-     * directedMove argument is in the form of { x, y, CrushUtilities.[DIRECTION] }
-     * 
-     * No checking is done for negative origin or destination cords.
-     * 
-     * @param directedMove A move in the form of {x, y, direction} int array.
-     * @return The block's cord where move's direction points in the form of
-     * 		{x, y} int array.
-     */ 
-    private static int[] getMoveDestination (int[] directedMove) 
-    {
-	    // Assume that destination cords is the same as origin cords.
-        int[] dest = { directedMove[0], directedMove[1] };
-  
-        int direction = directedMove[2];
-  
-        // Correct dest cords according to the move direction. 
-	    if (direction == CrushUtilities.LEFT) { 
-		    dest[0] -= 1;
-	    } 
-	    else if (direction == CrushUtilities.RIGHT) {
-		    dest[0] += 1;
-	    } 
-	    else if (direction == CrushUtilities.UP) {
-		    dest[1] += 1;
-	    } 
-	    else if (direction == CrushUtilities.DOWN) {
-		    dest[1] -= 1;
-	    }
-	  
-	    return dest;	  
-    }
+//==== Legacy code ====
+//  
+//    /** 
+//     * getMoveDestination() calculates the cords of the block next to the
+//     * given one, on the given direction.
+//     * 
+//     * directedMove argument is in the form of { x, y, CrushUtilities.[DIRECTION] }
+//     * 
+//     * No checking is done for negative origin or destination cords.
+//     * 
+//     * @param directedMove A move in the form of {x, y, direction} int array.
+//     * @return The block's cord where move's direction points in the form of
+//     * 		{x, y} int array.
+//     */ 
+//    private static int[] getMoveDestination (int[] directedMove) 
+//    {
+//	    // Assume that destination cords is the same as origin cords.
+//        int[] dest = { directedMove[0], directedMove[1] };
+//  
+//        int direction = directedMove[2];
+//  
+//        // Correct dest cords according to the move direction. 
+//	    if (direction == CrushUtilities.LEFT) { 
+//		    dest[0] -= 1;
+//	    } 
+//	    else if (direction == CrushUtilities.RIGHT) {
+//		    dest[0] += 1;
+//	    } 
+//	    else if (direction == CrushUtilities.UP) {
+//		    dest[1] += 1;
+//	    } 
+//	    else if (direction == CrushUtilities.DOWN) {
+//		    dest[1] -= 1;
+//	    }
+//	  
+//	    return dest;	  
+//    }
 }
